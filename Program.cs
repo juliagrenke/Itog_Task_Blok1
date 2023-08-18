a13bbfb7a0c17a845b5fc1e866a077ca41de2e73
juliagrenke <129645624+juliagrenke@users.noreply.github.com>
2023-08-18 19:12:11 +0500

    Исправлены ошибки кода при выводе строки

--- Program.cs
+++ Program.cs
@@ -45,9 +45,17 @@ void printA(string[] stringArray)
     Console.Write("[");
     for (int i = 0; i < stringArray.Length; i++)
     {
-        Console.Write($"{stringArray[i]}, ");
+        if (i != stringArray.Length - 1)
+        {
+            Console.Write($"{stringArray[i]}" + ", ");
+        }
+        else
+        {
+            Console.Write($"{stringArray[i]}" + "]");
+        }
+
     }
-    Console.Write("]");
+
 }
 array(stringArray);
 printA(symbol(stringArray));
